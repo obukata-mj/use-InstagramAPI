f26cbfea81297c522418f32eef7a1ea68a26670e
obukata <obukata@mj-star.jp>
2024-01-26 10:04:12 +0900

    ビジネスID・アクセストークンなど公開できない環境変数は.envで管理

--- config/snip/instagramapi.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$insta_version      = 'v18.0';
-	$insta_business_id  = '17841462430190554';
-	$insta_access_token = 'EAAEuN73PivcBO4bFHvcU9qpMDASyWZBEXF7XmchNLedaOk5Y0ABAdzhb1AvLfx1r3YOAT7Sb8uFls5CiBhMUbtAKxcWpyM93LZAQWJdRMMupXolcZAZAlfFVuPzWAWlVLG9Xuxomn9rj5ntLCDau6mIlXKMcQyLyY4rkfJmB6Nk2SrZCFkTAADmIoAUzoxSXc';
-
-	$json = file_get_contents("https://graph.facebook.com/{$insta_version}/{$insta_business_id}?fields=name%2Cmedia.limit({$insta_media_limit})%7Bcaption%2Cmedia_url%2Cthumbnail_url%2Cpermalink%2Clike_count%2Ccomments_count%2Ctimestamp%2Cmedia_type%7D&access_token={$insta_access_token}");
-	$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-?>
--- public/instagramapi.php
+++ public/instagramapi.php
@@ -10,7 +10,29 @@
  * @license MIT
  */
 
-  include_once "../config/snip/instagram.config.php";
+  /**
+   * .env環境変数からビジネスID、アクセストークンを取得
+   */
+  $envFilePath = __DIR__ . '/.env';
+  if (file_exists($envFilePath)) {
+    $envFile = file_get_contents($envFilePath);
+    $lines = explode("\n", $envFile);
+    foreach ($lines as $line) {
+      if (empty($line) || strpos($line, '#') === 0) {
+        continue;
+      }
+      list($key, $value) = explode('=', $line, 2);
+      putenv("$key=$value");
+    }
+  } else {
+    echo '.env ファイルが見つかりません。';
+  }
+
+  $insta_version      = 'v18.0';
+  $insta_business_id = getenv('INSTAGRAM_BUSINESS_ID');
+  $insta_access_token = getenv('INSTAGRAM_ACCESS_TOKEN');
+  $insta_media_limit  = 30;
+
   $json = file_get_contents("https://graph.facebook.com/{$insta_version}/{$insta_business_id}?fields=name%2Cmedia.limit({$insta_media_limit})%7Bcaption%2Cmedia_url%2Cthumbnail_url%2Cpermalink%2Clike_count%2Ccomments_count%2Ctimestamp%2Cmedia_type%7D&access_token={$insta_access_token}");
   $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
   echo $json;
